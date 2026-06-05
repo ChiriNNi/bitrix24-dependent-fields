e1aff4f4507e72635028a7eb38d8d918cd87976b
= <bekarysbulat@gmail.com>
2026-06-05 13:48:56 +0500

    Allow Bitrix24 iframe rendering

--- api/app.js
+++ api/app.js
@@ -2,15 +2,15 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 module.exports = async function handler(request, response) {
-  const postData = await readPostData(request);
-  const placementOptions = parsePlacementOptions(postData.PLACEMENT_OPTIONS);
+  const requestData = await readRequestData(request);
+  const placementOptions = parsePlacementOptions(requestData.PLACEMENT_OPTIONS);
   const html = fs.readFileSync(path.join(process.cwd(), "index.html"), "utf8");
   const injected = html.replace(
     "</head>",
     `<script>window.BITRIX_PLACEMENT_OPTIONS = ${JSON.stringify(placementOptions)};</script></head>`
   );
 
-  response.setHeader("Content-Type", "text/html; charset=utf-8");
+  setFrameHeaders(response);
   response.status(200).send(injected);
 };
 
@@ -26,10 +26,18 @@ function parsePlacementOptions(value) {
   }
 }
 
-function readPostData(request) {
+function readRequestData(request) {
   return new Promise((resolve) => {
+    const fromQuery = request.query || {};
+    const fromBody = request.body && typeof request.body === "object" ? request.body : {};
+
     if (request.method !== "POST") {
-      resolve({});
+      resolve(fromQuery);
+      return;
+    }
+
+    if (Object.keys(fromBody).length) {
+      resolve({ ...fromQuery, ...fromBody });
       return;
     }
 
@@ -38,7 +46,16 @@ function readPostData(request) {
       body += chunk;
     });
     request.on("end", () => {
-      resolve(Object.fromEntries(new URLSearchParams(body)));
+      resolve({ ...fromQuery, ...Object.fromEntries(new URLSearchParams(body)) });
     });
   });
+}
+
+function setFrameHeaders(response) {
+  response.setHeader("Content-Type", "text/html; charset=utf-8");
+  response.setHeader("X-Frame-Options", "ALLOWALL");
+  response.setHeader(
+    "Content-Security-Policy",
+    "frame-ancestors 'self' https://*.bitrix24.kz https://*.bitrix24.com https://*.bitrix24.ru"
+  );
 }
--- api/install.js
+++ api/install.js
@@ -5,5 +5,10 @@ module.exports = function handler(request, response) {
   const html = fs.readFileSync(path.join(process.cwd(), "install.html"), "utf8");
 
   response.setHeader("Content-Type", "text/html; charset=utf-8");
+  response.setHeader("X-Frame-Options", "ALLOWALL");
+  response.setHeader(
+    "Content-Security-Policy",
+    "frame-ancestors 'self' https://*.bitrix24.kz https://*.bitrix24.com https://*.bitrix24.ru"
+  );
   response.status(200).send(html);
 };
